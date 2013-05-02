ff02f5270c4c6a04b2924a08e6e03c01c69c896b
Abderrazak <bouadma.abderrazak@gmail.com>
2013-05-02 14:46:55 +0200

    creation of pcpPreference directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,21 @@
 /* Directives */
 
 
-angular.module('parapheur.preference.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('parapheur.preference.directives', [])
+    .directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+    .directive('pcpPreference',function(Preference){
+        return {
+            restrict:'E',
+            template:'<li ng-repeat="preference in Preference.query()">' +
+                '<p>{{element.elementLabel}}</p>' +
+                '<p>{{element.elementUIComponentType}}</p>' +
+                '</li>',
+            link:function(scope,elm,attrs){
+
+            }
+        }
+    });
