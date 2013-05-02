1b03ecb7e3b174d14b82dda63b7db6411bd3a59b
Abderrazak <bouadma.abderrazak@gmail.com>
2013-05-02 15:56:21 +0200

    fix preference directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,5 +5,4 @@
 angular.module('parapheur.preference.controllers', []).
   controller('PreferenceCtrl', function($scope,Preference) {
         $scope.greetingMessage = 'Preferences Module'
-        $scope.preferences = Preference.query();
   });
--- app/js/directives.js
+++ app/js/directives.js
@@ -12,12 +12,24 @@ angular.module('parapheur.preference.directives', [])
     .directive('pcpPreference',function(Preference){
         return {
             restrict:'E',
-            template:'<li ng-repeat="preference in Preference.query()">' +
-                '<p>{{element.elementLabel}}</p>' +
-                '<p>{{element.elementUIComponentType}}</p>' +
+            template:'<li ng-repeat="preference in preferences">' +
+                    '<h3 style="color: #0077b3">{{preference.title}}&nbsp;»</h3>' +
+                    '<p style="color: #c4e3f3">{{preference.hint}}</p>' +
+                    '<ul class="nav nav-tabs nav-stacked">'+
+                        '<pcp-element ng-model="preference"/>'+
+                    '</ul>'+
                 '</li>',
-            link:function(scope,elm,attrs){
-
+            link:function(scope,elm,attrs) {
+                scope.preferences = Preference.query();
             }
+        }
+    })
+    .directive('pcpElement',function(){
+        return {
+            restrict:'E',
+            template:'<li ng-repeat="element in preference.elements">' +
+                    '<p>{{element.elementLabel}}</p>' +
+                    '<p>{{element.elementUIComponentType}}</p>' +
+                '</li>'
         }
     });
